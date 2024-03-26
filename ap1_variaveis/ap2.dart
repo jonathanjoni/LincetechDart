b3fdd360b644201380a8b36573b8aa30c70efbab
jonathanjoni <jonathan-jona1@hotmail.com>
2024-03-26 12:28:14 -0300

    atividade 2 calculo media finalizada

---
+++ ap1_variaveis/ap2.dart
@@ -0,0 +1,10 @@
+void main() {
+  final nota1 = 7.0;
+  final nota2 = 8.0;
+  final nota3 = 5.3;
+  final nota4 = 9.7;
+
+  final calculoMedia = (nota1 + nota2 + nota3 + nota4) / 4;
+
+  print("A media entre [ $nota1 , $nota2 , $nota3 , $nota4 ] = $calculoMedia");
+}
